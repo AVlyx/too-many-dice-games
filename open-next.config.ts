0f7980805ffa6ad15671dca81bb2261490e5e13f
AVlyx <anton@vancleem.com>
2026-04-07 14:56:59 -0400

    deploy to cloudflare

--- open-next.config.ts
+++
@@ -1,4 +0,0 @@
-// OpenNext configuration stub — install @opennextjs/aws when deploying
-// import { defineConfig } from "@opennextjs/aws/config";
-// export default defineConfig({});
-export default {};
